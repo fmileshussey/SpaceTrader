c299538a80bd19ad49f266fe774c64518f7916e2
fmileshussey <fmileshussey@outlook.com>
2016-08-13 01:34:47 +1200

    Created basic JSON file for islands, started to flesh out class for Islands

---
+++ src/Island.java
@@ -0,0 +1,18 @@
+/**
+ * 12/08/2016
+ *
+ * @author Finnegan Miles-Hussey
+ */
+public class Island {
+    private final String name;
+    private final String type;
+    private final String description;
+
+    public Island(String name, String type, String description) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+    }
+
+
+}
---
+++ src/SeaTrader.java
@@ -0,0 +1,12 @@
+/**
+ * SeaTrader.java - A sea based trading game.
+ *
+ * @author Finnegan Miles-Hussey
+ * @version 1.0.0
+ */
+public class SeaTrader {
+    public static void main(String[] args) {
+
+    }
+}
+
