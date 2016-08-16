3e5e20539897d6be9793b2d114b2dbefcb9d18ee
fmileshussey <fmileshussey@outlook.com>
2016-08-16 15:50:43 +1200

    Created abstract class for Commodities & changed Island to an abstract class - more appropriate (I think, we'll see how it goes)

--- src/Island.java
+++ src/Island.java
@@ -3,35 +3,53 @@
  *
  * @author Finnegan Miles-Hussey
  */
-public class Island {
-    private final String name;
-    private final String[] charts_to;
-    private final String description;
-    private String[] resources;
+public abstract class Island {
+    private String name;
+    private String[] charts_to;
+    private String description;
 
-    public Island(String name, String[] charts_to, String description, String[] resources) {
-        this.name = name;
-        this.charts_to = charts_to;
-        this.description = description;
-        this.resources = resources;
-    }
-
+    /*
     public Island(String name, String[] charts_to, String description) {
         this.name = name;
         this.charts_to = charts_to;
         this.description = description;
     }
 
-    public String getDescription() {
-        return description;
+
+    /**
+     * Constructor for uninhabited islands.
+     *
+    public Island(String name, String[] charts_to, String description, String[] resources) {
+        this.name = name;
+        this.charts_to = charts_to;
+        this.description = description;
+        this.resources = resources;
+    }
+    */
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCharts_to(String[] charts_to) {
+        this.charts_to = charts_to;
     }
 
     public String getName() {
         return name;
     }
 
-    public String[] getResources() {
-        return resources;
+
+    public String[] getCharts_to() {
+        return charts_to;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
 
